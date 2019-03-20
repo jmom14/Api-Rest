3a3291110678fed0890d5a23a2870984fbb55745
jmom14 <manuel.marquina.14@gmail.com>
2019-03-19 23:00:45 -0600

    issue inserting publicacion

--- src/main/java/com/example/main/service/UsuarioService.java
+++ src/main/java/com/example/main/service/UsuarioService.java
@@ -10,10 +10,12 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.example.main.entities.Usuario;
 import com.example.main.repository.UsuarioRepository;
 
+@Service
 public class UsuarioService implements UserDetailsService {
 
 	@Autowired
